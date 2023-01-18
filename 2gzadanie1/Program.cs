fc839a7d95dce25c42911efe7c91517dbadb8194
PIBO0121038 Фомин Иван Константинович <PIBO0121038@sibupk.su>
2023-01-18 13:26:51 +0700

    Добавьте файлы проекта.

---
+++ 2gzadanie1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+namespace _2gzadanie1
+{
+    class OddEvenDemo
+    {
+        static void Main()
+        {
+            int number, delenie;
+            number = Int32.Parse(
+                Interaction.InputBox(
+                    "Введите целое число чтобы поделить на 3:")
+                );
+            
+            delenie = number/3;
+            string txt = "Ответ "+delenie+" !";
+            MessageBox.Show(txt);
+
+
+        }
+    }
+}
